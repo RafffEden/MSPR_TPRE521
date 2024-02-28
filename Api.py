e004222c930ccf6872c06c445e129a8fba7bfdf1
RafffEden <rafael.monteiro-matos@hotmail.com>
2024-02-28 23:15:51 +0100

    Api fonctionnel + ajout todolist

--- Api.py
+++ Api.py
@@ -1,27 +1,40 @@
-from flask import Flask, jsonify, request
+from flask import Flask, request, jsonify
 import pandas as pd
+from utillc import *
+import os
+from dotenv import dotenv_values 
 
 app = Flask(__name__)
+config = dotenv_values(".env")
+UPLOAD_FOLDER = config["UPLOAD_FOLDER"]
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
-# Read data from CSV file
-@app.route('/get_data', methods=['GET'])
-def get_data():
-    try:
-        df = pd.read_csv('info_especes.csv')
-        return jsonify(df.to_dict(orient='records'))
-    except FileNotFoundError:
-        return jsonify({'error': 'File not found'}), 404
+
+info = pd.read_csv("infos_especes.csv",delimiter=";")
+
+@app.route('/image_info/<prediction>')
+def get_image_info(prediction):
+    # Fetch information based on the prediction
+    if (info["Espece"] == prediction).any() :
+        json_info = info[info["Espece"] == prediction].to_dict(orient = "records")
+        return jsonify(json_info[0])
+    else:
+        return jsonify({'error': 'Prediction not found'}), 404
 
 # Post data to CSV file
 @app.route('/post_data', methods=['POST'])
-def post_data():
-    try:
-        data = request.json
-        df = pd.DataFrame(data)
-        df.to_csv('data.csv', mode='a', index=False, header=not bool(pd.read_csv('data.csv').shape[0]))
-        return jsonify({'success': True}), 201
-    except Exception as e:
-        return jsonify({'error': str(e)}), 400
+def upload_image():
+    if 'file' not in request.files:
+        return jsonify({'error': 'No file part'}), 400
+    file = request.files['file']
+    if file.filename == '':
+        return jsonify({'error': 'No selected file'}), 400
+    # Save the uploaded file
+    filename = 'uploaded_image.png'
+    # A ajouter l'image dans le dossier associé et les data dans le data.csv de dataiku 
+    img_path = os.path.join(app.config['UPLOAD_FOLDER'], filename) 
+    file.save(img_path)
+    return jsonify({'image_url': f'/uploads/{filename}'})
 
 if __name__ == '__main__':
     app.run(debug=True)
--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # MSPR_1
 Dépots pour la MSPR 1 
+
+
+## TODO 
+- récupérer le data.csv et la méthode qui permet de créer les données pour la prédiction et l'ajouter dans le post de l'api afin d'ajouter les nouvelles données aux dataset
